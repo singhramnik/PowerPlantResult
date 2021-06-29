e5f61da62047cc2e4fb252ae03c1828b247a8aab
Ramnik Singh <ramnik.net@gmail.com>
2021-06-29 16:30:17 +0100

    Refactored code to move the actual process out of main function.

---
+++ ConsoleClient/ProcessProvider/ProcessRunner.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using ConsoleClient.DataModel;
+using ConsoleClient.DataModel.Input;
+using ConsoleClient.DataModel.Output;
+using ConsoleClient.Calculators;
+using System.Collections.Generic;
+
+namespace ConsoleClient.ProcessProvider
+{
+    public class ProcessRunner
+    {
+        public void RunTask(string refDataFile, string inputPath, string outputPath)
+        {
+            var serialzer = new Serialzer();
+            var xmlRefData = File.ReadAllText(refDataFile);
+            var refData = serialzer.Deserializer<ReferenceData>(xmlRefData);
+
+            //Get list of Files
+            var files = Directory.GetFiles(inputPath);
+            foreach (var file in files)
+            {
+                var outputFileName = @$"{outputPath}\{Path.GetFileNameWithoutExtension(file)}-Result.xml";
+
+                ProcessSingleFile(refData, file, outputFileName);
+
+                //TODO: Once it has been Processed move it to Processed or Rejected folder
+            }
+        }
+
+        private void ProcessSingleFile(ReferenceData refData, string inputFile, string outputFileName)
+        {
+            var serialzer = new Serialzer();
+               
+            var xmlInputData = File.ReadAllText(inputFile);
+            var inputData = serialzer.Deserializer<GenerationReport>(xmlInputData);
+
+
+            var output = ProcessData(refData, inputData);
+
+            var outputStr = serialzer.Serialize(output);
+#if DEBUG
+            Console.WriteLine(outputStr);
+#endif
+
+            //At the moment it will just overwrite any existing file,
+            //good idea to suggest renaming old file if same file already exists
+            File.WriteAllText(outputFileName, outputStr);
+        }
+
+        /// <summary>
+        /// This is the main processor responsible for calling the write class for calculations. 
+        /// </summary>
+        private static GenerationOutput ProcessData(ReferenceData refData, GenerationReport inputData)
+        {
+            //Create output object for collection all the information. 
+            var output = new GenerationOutput
+            {
+                Totals = new List<DataModel.Output.Generator>()
+            };
+
+            ProcessWindData(refData, inputData, output);
+
+            output.MaxEmissionGenerators = new List<DataModel.Output.Day>();
+            ProcessGasData(refData, inputData, output);
+
+            output.ActualHeatRates = new List<ActualHeatRate>();
+            ProcessCoalData(refData, inputData, output);
+
+            return output;
+        }
+
+        private static void ProcessCoalData(ReferenceData refData, GenerationReport inputData, GenerationOutput output)
+        {
+            inputData.Coal.ForEach(c =>
+            {
+                var calculator = (CoalPlant)CalculatorFactory.GetPlantCalculator(c);
+                output.Totals.Add(new DataModel.Output.Generator
+                {
+                    Name = c.Name,
+                    Total = calculator.GetDailyGenerationValue(c, refData.Factors)
+                });
+
+                output.MaxEmissionGenerators.AddRange(calculator.GetDailyEmissions(c, refData.Factors));
+
+                output.ActualHeatRates.Add(new ActualHeatRate
+                {
+                    Name = c.Name,
+                    HeatRate = calculator.ActualHeatRate(c)
+                });
+            });
+        }
+
+        private static void ProcessGasData(ReferenceData refData, GenerationReport inputData, GenerationOutput output)
+        {
+            inputData.Gas.ForEach(g =>
+            {
+                GasPlant calculator = (GasPlant)CalculatorFactory.GetPlantCalculator(g);
+                output.Totals.Add(new DataModel.Output.Generator
+                {
+                    Name = g.Name,
+                    Total = calculator.GetDailyGenerationValue(g, refData.Factors)
+                });
+
+                output.MaxEmissionGenerators.AddRange(calculator.GetDailyEmissions(g, refData.Factors));
+            });
+        }
+
+        private static void ProcessWindData(ReferenceData refData, GenerationReport inputData, GenerationOutput output)
+        {
+            inputData.Wind.ForEach(w =>
+            {
+                var calculator = CalculatorFactory.GetPlantCalculator(w);
+
+                output.Totals.Add(new DataModel.Output.Generator
+                {
+                    Name = w.Name,
+                    Total = calculator.GetDailyGenerationValue(w, refData.Factors)
+                });
+            });
+        }
+    }
+}
--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Configuration;
-using System.IO;
-using ConsoleClient.DataModel;
-using ConsoleClient.DataModel.Input;
-using ConsoleClient.DataModel.Output;
-using ConsoleClient.Calculators;
+using ConsoleClient.ProcessProvider;
 
 namespace ConsoleClient
 {
@@ -12,87 +8,22 @@ namespace ConsoleClient
     {
         static void Main(string[] args)
         {
+            var refDataFile = ConfigurationManager.AppSettings["ReferenceDataFile"];
+            var inputPath = ConfigurationManager.AppSettings["DataInputFolder"];
+            var outputPath = ConfigurationManager.AppSettings["DataOutputFolder"];
 
-            var serialzer = new Serialzer();
-
-            var refDataPath = ConfigurationManager.AppSettings["ReferenceDataFile"];
-            var xmlRefData = File.ReadAllText(refDataPath);
-            var refData = serialzer.Deserializer<ReferenceData>(xmlRefData);
-
-            var inputPath = ConfigurationManager.AppSettings["DataInputFolder"] + @"\01-Basic.xml";
-
-            var xmlInputData = File.ReadAllText(inputPath);
-            var inputData = serialzer.Deserializer<GenerationReport>(xmlInputData);
-
-            var outputStr = ProcessData(refData, inputData);
-            
-            Console.WriteLine(outputStr);
+            if (args.Length > 0 && args[0].ToLower() == "-service")
+            {
+                //Call (new ProcessRunner()).RunTask() in an infinite Loop
+            }
+            else
+            {
+                //this can be run singly or as a scheduled task using window's scheduler
+                (new ProcessRunner()).RunTask(refDataFile, inputPath, outputPath);
+            }
 
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
-        }
-
-        private static string ProcessData(ReferenceData refData, GenerationReport inputData)
-        {
-            var outputPath = ConfigurationManager.AppSettings["DataOutputFolder"] + @"\01-Basic-Result.xml";
-
-            var serialzer = new Serialzer();
-
-            var output = new GenerationOutput
-            {
-                Totals = new System.Collections.Generic.List<DataModel.Output.Generator>()
-            };
-
-            inputData.Wind.ForEach(w => 
-            {
-                var calculator = CalculatorFactory.GetPlantCalculator(w);
-
-                output.Totals.Add(new DataModel.Output.Generator
-                {
-                    Name = w.Name,
-                    Total = calculator.GetDailyGenerationValue(w, refData.Factors)
-                });
-            });
-
-            output.MaxEmissionGenerators = new System.Collections.Generic.List<DataModel.Output.Day>();
-
-            inputData.Gas.ForEach(g =>
-            {
-                GasPlant calculator = (GasPlant)CalculatorFactory.GetPlantCalculator(g);
-                output.Totals.Add(new DataModel.Output.Generator 
-                {
-                    Name = g.Name,
-                    Total = calculator.GetDailyGenerationValue(g, refData.Factors)
-                });
-
-                output.MaxEmissionGenerators.AddRange(calculator.GetDailyEmissions(g, refData.Factors));
-            });
-
-            output.ActualHeatRates = new System.Collections.Generic.List<ActualHeatRate>();
-
-            inputData.Coal.ForEach(c =>
-            {
-                var calculator = (CoalPlant)CalculatorFactory.GetPlantCalculator(c);
-                output.Totals.Add(new DataModel.Output.Generator
-                {
-                    Name = c.Name,
-                    Total = calculator.GetDailyGenerationValue(c, refData.Factors)
-                });
-
-                output.MaxEmissionGenerators.AddRange(calculator.GetDailyEmissions(c, refData.Factors));
-
-                output.ActualHeatRates.Add(new ActualHeatRate
-                {
-                    Name = c.Name,
-                    HeatRate = calculator.ActualHeatRate(c)
-                });
-            });
-
-            var outputStr = serialzer.Serialize(output);
-
-            File.WriteAllText(outputPath, outputStr);
-
-            return outputStr;
         }
     }
 }
